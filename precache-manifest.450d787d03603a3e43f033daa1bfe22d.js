64bcd130d52b4f16b589b9873f87c6b7a699cb74
Lucas Mazo <lucasmazo32@gmail.com>
2020-05-30 16:28:31 -0500

    Updates

---
+++ precache-manifest.450d787d03603a3e43f033daa1bfe22d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bad9659466f6e1d4e8609b7483722184",
+    "url": "/otp/index.html"
+  },
+  {
+    "revision": "e56fa4f510b41942a932",
+    "url": "/otp/static/css/main.20dc9832.chunk.css"
+  },
+  {
+    "revision": "41b69db64add2388ced3",
+    "url": "/otp/static/js/2.ed2444c5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/otp/static/js/2.ed2444c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e56fa4f510b41942a932",
+    "url": "/otp/static/js/main.98a8e31c.chunk.js"
+  },
+  {
+    "revision": "ab15ca40c05c8296028a",
+    "url": "/otp/static/js/runtime-main.bc4279dc.js"
+  },
+  {
+    "revision": "cdee11cdb8c910b96962935608cb5900",
+    "url": "/otp/static/media/circle-logo.cdee11cd.png"
+  }
+]);
